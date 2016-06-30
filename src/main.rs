21e6514224785e7d8e6b75cee527f4fd404d05ce
Martin Thoresen <martinhath@gmail.com>
2016-06-30 23:10:17 +0200

    Implement encode, and error checking

--- src/main.rs
+++ src/main.rs
@@ -22,23 +22,6 @@ impl SquareMatrix {
     }
 }
 
-// NOTE: encoding
-fn sample_matrix() -> SquareMatrix {
-    let mut vec = vec![52f32, 55f32, 61f32, 66f32, 70f32, 61f32, 64f32, 73f32, 63f32, 59f32,
-                       55f32, 90f32, 109f32, 85f32, 69f32, 72f32, 62f32, 59f32, 68f32, 113f32,
-                       144f32, 104f32, 66f32, 73f32, 63f32, 58f32, 71f32, 122f32, 154f32, 106f32,
-                       70f32, 69f32, 67f32, 61f32, 68f32, 104f32, 126f32, 88f32, 68f32, 70f32,
-                       79f32, 65f32, 60f32, 70f32, 77f32, 68f32, 58f32, 75f32, 85f32, 71f32,
-                       64f32, 59f32, 55f32, 61f32, 65f32, 83f32, 87f32, 79f32, 69f32, 68f32,
-                       65f32, 76f32, 78f32, 94f32];
-
-    SquareMatrix {
-        dimention: 8,
-        values: vec,
-    }
-}
-
-// NOTE: encoding
 fn discrete_cosine_transform(mat: &SquareMatrix) -> SquareMatrix {
     let alpha = |u| {
         if u == 0 {
@@ -119,20 +102,8 @@ fn discrete_cosine_transform_inverse(mat: &SquareMatrix) -> SquareMatrix {
     }
 }
 
-fn quantization_matrix() -> SquareMatrix {
-    SquareMatrix {
-        dimention: 8,
-        values: vec![16f32, 11f32, 10f32, 16f32, 24f32, 40f32, 51f32, 61f32, 12f32, 12f32, 14f32,
-                     19f32, 26f32, 58f32, 60f32, 55f32, 14f32, 13f32, 16f32, 24f32, 40f32, 57f32,
-                     69f32, 56f32, 14f32, 17f32, 22f32, 29f32, 51f32, 87f32, 80f32, 62f32, 18f32,
-                     22f32, 37f32, 56f32, 68f32, 109f32, 103f32, 77f32, 24f32, 35f32, 55f32,
-                     64f32, 81f32, 104f32, 113f32, 92f32, 49f32, 64f32, 78f32, 87f32, 103f32,
-                     121f32, 120f32, 101f32, 72f32, 92f32, 95f32, 98f32, 112f32, 100f32, 103f32,
-                     99f32],
-    }
-}
-
-fn inner_div(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
+// TODO: do this not retarded
+fn inner_div_round(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
     let d = a.dimention;
     if d != b.dimention {
         panic!("Matrix dimentions must be the same");
@@ -141,7 +112,7 @@ fn inner_div(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
     for j in 0..d {
         for i in 0..d {
             let index = j * d + i;
-            vec.push(a.values[index] / b.values[index]);
+            vec.push((a.values[index] / b.values[index]).round());
         }
     }
     SquareMatrix {
@@ -150,6 +121,7 @@ fn inner_div(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
     }
 }
 
+// TODO: do this not retarded
 fn inner_mul(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
     let d = a.dimention;
     if d != b.dimention {
@@ -169,23 +141,46 @@ fn inner_mul(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
 }
 
 fn decode(mat: SquareMatrix) -> SquareMatrix {
+    println!("decode()");
+    mat.print();
     let dequantized = inner_mul(&mat, &quantization_matrix());
-    println!("dequantized");
-    dequantized.print();
-    // name?
     let mut spatial = discrete_cosine_transform_inverse(&dequantized);
-    println!("after transform and round");
     for n in spatial.values.iter_mut() {
         *n = n.round();
     }
-    spatial.print();
-
     for n in spatial.values.iter_mut() {
         *n += 128f32;
     }
-    println!("Add back 128");
     spatial.print();
     spatial
+}
+
+fn encode(mat: SquareMatrix) -> SquareMatrix {
+    println!("encode()");
+    mat.print();
+    let mut mat = mat;
+    for n in mat.values.iter_mut() {
+        *n -= 128f32;
+    }
+    let transformed = discrete_cosine_transform(&mat);
+    let quantized = inner_div_round(&transformed, &quantization_matrix());
+    quantized.print();
+    quantized
+}
+
+
+
+fn quantization_matrix() -> SquareMatrix {
+    SquareMatrix {
+        dimention: 8,
+        values: vec![16f32, 11f32, 10f32, 16f32, 24f32, 40f32, 51f32, 61f32, 12f32, 12f32, 14f32,
+                     19f32, 26f32, 58f32, 60f32, 55f32, 14f32, 13f32, 16f32, 24f32, 40f32, 57f32,
+                     69f32, 56f32, 14f32, 17f32, 22f32, 29f32, 51f32, 87f32, 80f32, 62f32, 18f32,
+                     22f32, 37f32, 56f32, 68f32, 109f32, 103f32, 77f32, 24f32, 35f32, 55f32,
+                     64f32, 81f32, 104f32, 113f32, 92f32, 49f32, 64f32, 78f32, 87f32, 103f32,
+                     121f32, 120f32, 101f32, 72f32, 92f32, 95f32, 98f32, 112f32, 100f32, 103f32,
+                     99f32],
+    }
 }
 
 fn encoded_matrix() -> SquareMatrix {
@@ -200,20 +195,41 @@ fn encoded_matrix() -> SquareMatrix {
     }
 }
 
+fn sample_matrix() -> SquareMatrix {
+    SquareMatrix {
+        dimention: 8,
+        values: vec![52f32, 55f32, 61f32, 66f32, 70f32, 61f32, 64f32, 73f32, 63f32, 59f32, 55f32,
+                     90f32, 109f32, 85f32, 69f32, 72f32, 62f32, 59f32, 68f32, 113f32, 144f32,
+                     104f32, 66f32, 73f32, 63f32, 58f32, 71f32, 122f32, 154f32, 106f32, 70f32,
+                     69f32, 67f32, 61f32, 68f32, 104f32, 126f32, 88f32, 68f32, 70f32, 79f32,
+                     65f32, 60f32, 70f32, 77f32, 68f32, 58f32, 75f32, 85f32, 71f32, 64f32, 59f32,
+                     55f32, 61f32, 65f32, 83f32, 87f32, 79f32, 69f32, 68f32, 65f32, 76f32, 78f32,
+                     94f32],
+    }
+}
+
+fn error_matrix(a: &SquareMatrix, b: &SquareMatrix) -> SquareMatrix {
+    let d = a.dimention;
+    let mut vec = Vec::with_capacity(d * d);
+    for y in 0..d {
+        for x in 0..d {
+            let index = y * d + x;
+            vec.push(a.values[index] - b.values[index]);
+        }
+    }
+    SquareMatrix {
+        dimention: d,
+        values: vec,
+    }
+}
+
 fn main() {
-    let mut matrix = encoded_matrix();// sample_matrix();
-    matrix.print();
+    let mut matrix = sample_matrix();
+    let encoded = encode(matrix);
+    let decoded = decode(encoded);
 
-    decode(matrix);
+    let error = error_matrix(&sample_matrix(), &decoded);
+    error.print();
 
-    //     for a in matrix.values.iter_mut() {
-    //         *a -= 128f32;
-    //     }
-    //     matrix.print();
-    //     println!("");
-    //
-    //     let mut transformed = discrete_cosine_transform(&matrix);
-    //
-    // transformed.print();
     println!("");
 }
